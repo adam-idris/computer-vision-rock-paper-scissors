25531054b1fe7a69ec9f22d825f9c8c73a7486aa
Idris <adam.idris@live.co.uk>
2022-11-24 14:38:53 +0000

    fix

--- manual_rps.py
+++ manual_rps.py
@@ -10,23 +10,28 @@ def get_user_choice():
     return user_choice
 
 def get_winner(computer_choice, user_choice):
-    if computer_choice == 'Rock' and user_choice == 'Scissors':
-        print("You lost")
-
-    elif computer_choice == 'Rock' and user_choice == 'Paper':
-        print("You won")
-
-    elif computer_choice == 'Paper' and user_choice == 'Scissors':
-        print("You won")
-
-    elif computer_choice == 'Paper' and user_choice == 'Rock':
-        print("You lost")
-
-    elif computer_choice == 'Scissors' and user_choice == 'Rock':
-        print("You won")
-
-    elif computer_choice == 'Scissors' and user_choice == 'Paper':
-        print("You lost")
-
-    else:
+    
+    if computer_choice == user_choice:
         print("It is a tie!")
+    elif computer_choice == 'Rock':
+        if user_choice == 'Scissors':
+            print("You lost")
+        elif user_choice == 'Paper':
+            print("You won")
+    elif computer_choice == 'Paper':
+        if user_choice == 'Scissors':
+            print("You won")
+        elif user_choice == 'Rock':
+            print("You lost")
+    elif computer_choice == 'Scissors':
+        if user_choice == 'Rock':
+            print("You won")
+        elif user_choice == 'Paper':
+            print("You lost")
+
+def play():
+    get_computer_choice()
+    get_user_choice()
+    get_winner(get_computer_choice, get_user_choice)
+
+play()
