5872c71b8504795b8076537674ffb4b6de690fba
Idris <adam.idris@live.co.uk>
2022-11-24 15:37:00 +0000

    fix

--- manual_rps.py
+++ manual_rps.py
@@ -10,25 +10,25 @@ def get_user_choice():
     return user_choice
 
 def get_winner(computer_choice, user_choice):
-    
     if computer_choice == user_choice:
         print("It is a tie!")
+    
     elif computer_choice == 'Rock':
-
         if user_choice == 'Scissors':
             print("You lost")
         elif user_choice == 'Paper':
             print("You won")
 
     elif computer_choice == 'Paper':
-
         if user_choice == 'Scissors':
             print("You won")
         elif user_choice == 'Rock':
             print("You lost")
-    
+
     elif computer_choice == 'Scissors':
         if user_choice == 'Rock':
             print("You won")
         elif user_choice == 'Paper':
             print("You lost")
+
+get_winner('Paper', 'Paper')
